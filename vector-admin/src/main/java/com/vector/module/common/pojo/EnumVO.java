b38e2bd0f9e3600d65c0cabd1d983a87984613be
wengxs <754543850@qq.com>
2024-05-13 01:26:02 +0800

    build system module

---
+++ vector-admin/src/main/java/com/vector/module/common/pojo/EnumVO.java
@@ -0,0 +1,16 @@
+package com.vector.module.common.pojo;
+
+import lombok.Data;
+
+/**
+ * 枚举视图
+ * @author wengxs
+ * @date 2024/5/12
+ */
+@Data
+public class EnumVO {
+
+    private String value;
+
+    private String label;
+}
