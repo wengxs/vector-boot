6cc7830abe2fc0c3c32418638dcd08726a13bf45
wengxs <754543850@qq.com>
2025-04-03 15:40:57 +0800

    更新文件上传URI

--- vector-admin/src/main/java/com/vector/module/common/controller/FileController.java
+++ vector-admin/src/main/java/com/vector/module/common/controller/FileController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
  * @date 2024/5/5
  */
 @RestController
-@RequestMapping("/common/file")
+@RequestMapping("/third/file")
 public class FileController {
 
     @Autowired
